0084b2d57e502beb4fc92a511c5a66780e7f98a6
Roby Codes <robert.cfx@proton.me>
2024-05-30 22:51:20 +0200

    feat(template): Added template.tsx to animate pages on load

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,3 +1,18 @@
-const Template = ({ children }: RootLayoutProps) => <>{children}</>;
+import { motion } from "framer-motion";
+const Template = ({ children }: RootLayoutProps) => (
+  <motion.div
+    initial={{
+      opacity: 0,
+    }}
+    animate={{
+      opacity: 1,
+    }}
+    transition={{
+      duration: 0.5,
+    }}
+  >
+    {children}
+  </motion.div>
+);
 
 export default Template;
